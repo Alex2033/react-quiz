39263ec774b8bd1150dd88f8d95cee1949230454
Alexandr <XoxlofAlexandr@yandex.ru>
2020-08-16 22:11:52 +0300

    создание страниц

---
+++ src/containers/QuizList/QuizList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class QuizList extends Component {
+    render() {
+        return (
+            <h1>Quiz list</h1>
+        );
+    }
+}
+
+export default QuizList;
