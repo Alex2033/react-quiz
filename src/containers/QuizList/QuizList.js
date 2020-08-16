9768d10fef6c31b9f3d5f08f2c62f59418c8ce0a
Alexandr <XoxlofAlexandr@yandex.ru>
2020-08-16 22:41:26 +0300

    Добавлена страница списка

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import classes from './QuizList.module.scss';
+import { NavLink } from 'react-router-dom';
 
 class QuizList extends Component {
+
+    renderQuizes = () => {
+        return [1, 2, 3].map((quiz, index) => {
+            return (
+                <li 
+                    key={index}
+                >
+                    <NavLink to={'/quiz/' + quiz}>
+                        Тест {quiz}
+                    </NavLink>
+                </li>
+            )
+        })
+    }
+
     render() {
         return (
-            <h1>Quiz list</h1>
+            <div className={classes.quizList}>
+                <div>
+                    <h1>Список тестов</h1>
+
+                    <ul>
+                        { this.renderQuizes() }
+                    </ul>
+                </div>
+                
+            </div>
         );
     }
 }
