7b244479edcc9b47f0d1bd42ac3fae9c3b82e6a5
Alexandr <XoxlofAlexandr@yandex.ru>
2020-09-21 17:11:24 +0300

    js-расширения заменены на jsx

--- src/components/UI/Backdrop/Backdrop.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classes from './Backdrop.module.scss';
-
-
-const Backdrop = (props) => (
-          <div className={classes.backdrop} onClick={props.onClick}></div>  
-    );
-
-export default Backdrop;
---
+++ src/containers/Auth/Auth.jsx
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import classes from "./Auth.module.scss";
+import Button from "../../components/UI/Button/Button";
+import Input from "../../components/UI/Input/Input";
+
+class Auth extends Component {
+  state = {
+    formControls: {
+      email: {
+        value: "",
+        type: "email",
+        label: "Email",
+        errorMessage: "Введите корректный email",
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true,
+        },
+      },
+      password: {
+        value: "",
+        type: "password",
+        label: "Пароль",
+        errorMessage: "Введите корректный пароль",
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6,
+        },
+      },
+    },
+  };
+
+  loginHandler = () => {};
+
+  registerHandler = () => {};
+
+  submitHandler = (event) => {
+    event.preventDefault();
+  };
+
+  onChangeHandler = (event, controlName) => {
+    console.log(`${controlName}: `, event.target.value);
+
+    const formControls = { ...this.state.formControls };
+    const control = { ...formControls[controlName] };
+
+    constrol.value = even.target.value;
+    control.touched = true;
+    control.valid = this.validate(control.value.control.validation);
+  };
+
+  renderInputs() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName];
+      return (
+        <Input
+          key={controlName + index}
+          label={control.label}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          shouldValidate={!!control.validation}
+          errorMessage={control.errorMessage}
+          onchange={(event) => this.onChangeHandler(event, controlName)}
+        />
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div className={classes.auth}>
+        <div>
+          <h1>Авторизация</h1>
+
+          <form onSubmit={this.submitHandler} className={classes.authForm}>
+            {this.renderInputs()}
+
+            <Button type="success" onClick={this.loginHandler}>
+              Войти
+            </Button>
+            <Button type="primary" onClick={this.registerHandler}>
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Auth;
