5d9f0d177898e78ff88e53ddf24a8e79c6648e22
Alexandr <XoxlofAlexandr@yandex.ru>
2020-09-21 21:38:18 +0300

    создание компонента селекта

--- src/containers/QuizCreator/QuizCreator.jsx
+++ src/containers/QuizCreator/QuizCreator.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import classes from "./QuizCreator.module.scss";
 import Button from "../../components/UI/Button/Button";
+import Select from "../../components/UI/Select/Select";
 import Input from "../../components/UI/Input/Input";
 import { createControl } from "../../form/formFramework";
 import Auxillary from "../../hoc/Auxillary/Auxillary";
@@ -36,6 +37,7 @@ class QuizCreator extends Component {
   state = {
     quiz: [],
     formControls: createFormControls(),
+    rightAnswerId: 1,
   };
 
   submitHandler = (event) => {
@@ -71,7 +73,25 @@ class QuizCreator extends Component {
     });
   }
 
+  selectChangeHandler = (event) => {
+    this.setState({ rightAnswerId: +event.target.value });
+  };
+
   render() {
+    const select = (
+      <Select
+        label="Выберите правильный ответ"
+        value={this.state.rightAnswerId}
+        onChange={this.selectChangeHandler}
+        options={[
+          { text: 1, value: 1 },
+          { text: 2, value: 2 },
+          { text: 3, value: 3 },
+          { text: 4, value: 4 },
+        ]}
+      />
+    );
+
     return (
       <div className={classes.QuizCreator}>
         <div>
@@ -80,7 +100,7 @@ class QuizCreator extends Component {
           <form onSubmit={this.submitHandler}>
             {this.renderControls()}
 
-            <select></select>
+            {select}
 
             <Button type="primary" onClick={this.onAddQuestionHandler}>
               Добавить вопрос
