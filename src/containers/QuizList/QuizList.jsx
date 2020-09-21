68d2cecb1f0fc97a3f727e4d6ecf81cfe371e1c9
Alexandr <XoxlofAlexandr@yandex.ru>
2020-09-21 22:17:10 +0300

    добавление axios

--- src/containers/QuizList/QuizList.jsx
+++ src/containers/QuizList/QuizList.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import classes from "./QuizList.module.scss";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 class QuizList extends Component {
   renderQuizes = () => {
@@ -12,6 +13,12 @@ class QuizList extends Component {
       );
     });
   };
+
+  componentDidMount() {
+    axios
+      .get("https://react-quiz-411e0.firebaseio.com/quiz.json")
+      .then((response) => console.log(response));
+  }
 
   render() {
     return (
