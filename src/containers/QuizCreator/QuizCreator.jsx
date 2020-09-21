29c26971f8383c91a38120f4ed1936096b2e5071
Alexandr <XoxlofAlexandr@yandex.ru>
2020-09-21 20:40:02 +0300

    создание контролов

--- src/containers/QuizCreator/QuizCreator.jsx
+++ src/containers/QuizCreator/QuizCreator.jsx
@@ -2,8 +2,42 @@ import React, { Component } from "react";
 import classes from "./QuizCreator.module.scss";
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
+import { createControl } from "../../form/formFramework";
+import Auxillary from "../../hoc/Auxillary/Auxillary";
+
+function createOptionControl(number) {
+  return createControl(
+    {
+      label: `Вариант ${number}`,
+      errorMessage: "Значение не может быть пустым",
+      id: number,
+    },
+    { required: true }
+  );
+}
+
+function createFormControls() {
+  return {
+    question: createControl(
+      {
+        label: "Введите вопрос",
+        errorMessage: "Вопрос не может быть пустым",
+      },
+      { requited: true }
+    ),
+    option1: createOptionControl(1),
+    option2: createOptionControl(2),
+    option3: createOptionControl(3),
+    option4: createOptionControl(4),
+  };
+}
 
 class QuizCreator extends Component {
+  state = {
+    quiz: [],
+    formControls: createFormControls(),
+  };
+
   submitHandler = (event) => {
     event.preventDefault();
   };
@@ -12,6 +46,31 @@ class QuizCreator extends Component {
 
   createQuizHandler = () => {};
 
+  changeHandler = (value, controlName) => {};
+
+  renderControls() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName];
+
+      return (
+        <Auxillary key={controlName + index}>
+          <Input
+            label={control.label}
+            value={control.value}
+            valid={control.valid}
+            shouldValidate={!!control.validation}
+            touched={control.touched}
+            errorMessage={control.errorMessage}
+            onChange={(event) =>
+              this.changeHandler(event.target.value, controlName)
+            }
+          />
+          {index === 0 ? <hr /> : null}
+        </Auxillary>
+      );
+    });
+  }
+
   render() {
     return (
       <div className={classes.QuizCreator}>
@@ -19,12 +78,7 @@ class QuizCreator extends Component {
           <h1>Создание теста</h1>
 
           <form onSubmit={this.submitHandler}>
-            <Input />
-            <hr />
-            <Input />
-            <Input />
-            <Input />
-            <Input />
+            {this.renderControls()}
 
             <select></select>
 
